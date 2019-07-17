022d190b0fc3b6a3ed8bb07a20cac98ca4a3b94e
Tomasz Sienkiewicz <tomasz.sienkiewicz@mail.com>
2019-07-17 12:40:01 +0200

    add scss and js

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
@@ -20,3 +20,17 @@ if ('serviceWorker' in navigator) {
 console.log(`Hello world!`);
 
 
+const message = document.querySelector('.form__message--js');
+const load = document.querySelector('.form__button--load--js');
+const save = document.querySelector('.form__button--save--js');
+
+save.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log(message.value);
+  localStorage.setItem('message', message.value);
+});
+
+load.addEventListener('click', (e) => {
+  e.preventDefault();
+  message.innerHTML = localStorage.getItem('message');
+});
